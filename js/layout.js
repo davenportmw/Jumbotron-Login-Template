cc50645c1622878243b801f3081fa7de9ed08bd4
Matthew Davenport <mada6251@colorado.edu>
2019-07-29 20:21:41 -0600

    Initial commit.

---
+++ js/layout.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function goToLogin() {
+    window.location.href = "http://localhost:8000/views/login.html";
+}
+
+function goToIndex() {
+    window.location.href = "http://localhost:8000/index.html";
+}
+
+function hideModal() {
+    $('#modalId').modal('hide');
+}
+
+function hideSpinner() {
+    document.getElementById("spin1").style.visibility = "hidden";
+}
+
+function showSpinner() {
+    document.getElementById("spin1").style.visibility = "visible"; 
+}
